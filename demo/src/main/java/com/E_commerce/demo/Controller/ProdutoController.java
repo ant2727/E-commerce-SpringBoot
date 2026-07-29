7029b83c08f975a37aa12fe3611f135875e9ee40
ant2727 <antonio.bernardi356@gmail.com>
2026-07-29 07:52:39 -0300

    feat: cria endpoint para listar produtos

--- demo/src/main/java/com/E_commerce/demo/Controller/ProdutoController.java
+++ demo/src/main/java/com/E_commerce/demo/Controller/ProdutoController.java
@@ -1,16 +1,26 @@
 package com.E_commerce.demo.Controller;
 
+import com.E_commerce.demo.Entity.Produto;
+import com.E_commerce.demo.Service.ProdutoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
-    @GetMapping
-    public String listarProdutos() {
-        return "Lista de produtos";
+
+    private final ProdutoService service;
+
+    public ProdutoController(ProdutoService service) {
+        this.service = service;
     }
 
+    @GetMapping
+    public List<Produto> listarTodos() {
+        return service.listarTodos();
+    }
 }
