64fc4ab8adf3a0b72c110c40b46a10da086aca5f
ant2727 <antonio.bernardi356@gmail.com>
2026-07-29 07:45:12 -0300

    feat: cria ProdutoService

---
+++ demo/src/main/java/com/E_commerce/demo/Service/ProdutoService.java
@@ -0,0 +1,19 @@
+package com.E_commerce.demo.Service;
+
+import com.E_commerce.demo.Entity.Produto;
+import com.E_commerce.demo.Repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class ProdutoService {
+
+        private final ProdutoRepository repository;
+
+        public ProdutoService(ProdutoRepository repository) {
+            this.repository = repository;
+        }
+
+        public List<Produto> listarTodos() {
+            return repository.findAll();
+        }
+}
