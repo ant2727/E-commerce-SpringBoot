c33b470bed0627f61728149bb1dbba56827e8ee0
ant2727 <antonio.bernardi356@gmail.com>
2026-07-31 09:55:00 -0300

    feat: adiciona validação de upload de imagens

--- demo/src/main/java/com/E_commerce/demo/service/FileStorageService.java
+++ demo/src/main/java/com/E_commerce/demo/service/FileStorageService.java
@@ -3,6 +3,8 @@ package com.E_commerce.demo.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,6 +19,26 @@ public class FileStorageService {
 
         if (arquivo.isEmpty()) {
             throw new IllegalArgumentException("Arquivo vazio.");
+        }
+        if (arquivo.getSize() > 5 * 1024 * 1024) {
+            throw new IllegalArgumentException("A imagem deve ter no máximo 5 MB.");
+        }
+        String nome = arquivo.getOriginalFilename();
+
+        if (nome == null ||
+                !(nome.toLowerCase().endsWith(".png") ||
+                        nome.toLowerCase().endsWith(".jpg") ||
+                        nome.toLowerCase().endsWith(".jpeg") ||
+                        nome.toLowerCase().endsWith(".gif") ||
+                        nome.toLowerCase().endsWith(".webp"))) {
+
+            throw new IllegalArgumentException("Formato de imagem não permitido.");
+        }
+
+        BufferedImage imagem = ImageIO.read(arquivo.getInputStream());
+
+        if (imagem == null) {
+            throw new IllegalArgumentException("O arquivo enviado não é uma imagem válida.");
         }
 
         String tipo = arquivo.getContentType();
