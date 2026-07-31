c04e569734a933c890c6727e36830ebc7d5c7b20
ant2727 <antonio.bernardi356@gmail.com>
2026-07-31 12:48:53 -0300

    feat: adiciona entidade cliente

---
+++ demo/src/main/java/com/E_commerce/demo/entity/Cliente.java
@@ -0,0 +1,56 @@
+package com.E_commerce.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    public Cliente() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
