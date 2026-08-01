95ac6796ad6f1963ae6e924deaf79fd6a042f9a5
ant2727 <antonio.bernardi356@gmail.com>
2026-08-01 09:09:10 -0300

    feat: adicionaa ItemRequest e CarrinhoController

---
+++ demo/src/main/java/com/E_commerce/demo/controller/CarrinhoController.java
@@ -0,0 +1,31 @@
+package com.E_commerce.demo.controller;
+
+
+import com.E_commerce.demo.dto.request.AdicionarItemRequest;
+import com.E_commerce.demo.service.CarrinhoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/carrinhos")
+@RequiredArgsConstructor
+public class CarrinhoController {
+
+    private final CarrinhoService service;
+
+    @PostMapping("/adicionar")
+    public ResponseEntity<Void> adicionarProduto(
+            @Valid @RequestBody AdicionarItemRequest request) {
+
+        service.adicionarProduto(
+                request.getClienteId(),
+                request.getProdutoId(),
+                request.getQuantidade()
+        );
+
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ demo/src/main/java/com/E_commerce/demo/dto/request/AdicionarItemRequest.java
@@ -0,0 +1,26 @@
+package com.E_commerce.demo.dto.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class AdicionarItemRequest {
+
+    @NotNull(message = "O cliente é obrigatório.")
+    private Long clienteId;
+
+    @NotNull(message = "O produto é obrigatório.")
+    private Long produtoId;
+
+    @NotNull(message = "A quantidade é obrigatória.")
+    @Min(value = 1, message = "A quantidade deve ser maior que zero.")
+    private Integer quantidade;
+
+    public AdicionarItemRequest() {
+    }
+
+    }
